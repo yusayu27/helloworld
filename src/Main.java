1f5b51a3208490dc0401015b0dc6780acb868f9e
Yusuf Sahin <yusuf.s@gmx.de>
2024-09-30 17:41:30 +0200

    Add a println to the Main class.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is my first program on git!");
+        System.out.println("This is change to the Initial commit!");
     }
 }
